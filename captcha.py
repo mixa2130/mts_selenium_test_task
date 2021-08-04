2b6b9bba043b73a4d688886922d752a6c8a6ea05
mixa2130 <mixa21.11@mail.ru>
2021-08-04 20:47:11 +0300

    Full code refactoring: linter issues, doc strings, requirements, .gitignore

--- captcha.py
+++ captcha.py
@@ -1,7 +1,9 @@
-from PIL import Image
-import pytesseract
+"""This module is responsible for interaction with tesseract-ocr"""
 import re
 import os
+import pytesseract
+
+from PIL import Image
 from dotenv import load_dotenv
 
 # Tesseract windows configuration
@@ -9,8 +11,14 @@ load_dotenv()
 pytesseract.pytesseract.tesseract_cmd = os.path.join(os.getenv('TESSERACT_DIR_LOCATION'), 'tesseract.exe')
 
 
-def get_text_from_captcha(filename):
+def get_text_from_captcha(filename: str) -> str:
+    """
+    Using tesseract-ocr extracts text from captcha image.
+
+    Note: Fssp uses a standard captcha with a length of 5 characters.
+    """
     text: str = pytesseract.image_to_string(Image.open(filename), lang='rus')
     letters = re.findall(r'\w', text)
-    capcha_text = ''.join(letters)
-    return capcha_text[:5].lower()
+    captcha_text: str = ''.join(letters)
+
+    return captcha_text[:5].lower()
--- fssp.py
+++ fssp.py
@@ -1,34 +1,56 @@
-from typing import NamedTuple
+"""This module is responsible for processing fssp(https://fssp.gov.ru/) website"""
+
 import urllib.request
-import time
+from typing import Deque
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common import keys
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
 from captcha import get_text_from_captcha
-from typing import List, Deque
 from excel_interaction import write_excel_file, InputArgs, read_excel_file
 
 
 class SessionFssp:
+    """Main class for interacting  with https://fssp.gov.ru website"""
+
     def __init__(self):
         self.browser = webdriver.Firefox()
         self.wait = WebDriverWait(self.browser, 6)
-        self.browser.implicitly_wait(10)
 
+        self.browser.implicitly_wait(10)
         self.browser.get('https://fssp.gov.ru/')
         info_close_button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.tingle-modal__close')))
         info_close_button.click()
 
-    def get_debts(self, args: InputArgs, debts: list) -> bool:
+    def _restart_sesion(self):
+        self.browser.get('https://fssp.gov.ru/')
+
+    def get_debts(self, args: InputArgs, all_debts: list) -> bool:
+        """
+        Gets all debts from fssp website for the specified arguments in args param.
+
+        How it works:
+        Adds to the all_debts - tuple of debt values. If there are no debts -
+        (name, "Нет задолженностей").
+
+        :param args: search arguments
+        :param all_debts: the list, to which the result will be sent
+
+        :return: True - debts found/
+        False - failed, we didn't meet the deadline, perhaps the site is lagging
+        """
+
         def get_text(search_el):
+            """Retrieves text from chosen html block"""
             return search_el.text
 
-        start_page: bool = True
+        start_page: bool = True  # понадоится если мы захотим ,по каким-то причинам,
+        # продолжить поиск со стартовой страницы
         try:
-            # Advanced searching available only at the start page
+            # Кнопка расширенного поиска доступна только на стартовой странице
             big_search_btn = self.browser.find_element_by_css_selector('a.btn.btn-light')
             big_search_btn.click()
         except NoSuchElementException:
@@ -47,61 +69,83 @@ class SessionFssp:
             last_form.send_keys(keys.Keys.ENTER)
 
         if self._solve_captcha() == -1:
+            self._restart_sesion()
             return False
 
         try:
             res_table = self.browser.find_element_by_css_selector('div.results')
         except NoSuchElementException:
+            # Долгий процессинг каптч, не уложились во времени - стоит начать заново со стартовой страницы
+            self._restart_sesion()
             return False
 
         try:
             res_body = res_table.find_element_by_css_selector('div.results-frame').find_element_by_css_selector('tbody')
         except NoSuchElementException:
-            debts.append((' '.join(args_dict.values()), 'Нет задолженностей'))
+            # У данного человека нет задолженностей
+            all_debts.append((' '.join(args_dict.values()), 'Нет задолженностей'))
             return True
 
-        # Results exists
-        debt_info = res_body.find_elements_by_tag_name('td')
+        # Долги есть
+        debt_info: list = res_body.find_elements_by_tag_name('td')
+        # debt_info содержит:
+        # ячейки с названием республики, к которой относится данный долг
+        # ячейки с информацией о задолженностях
         counter: int = 0
 
         while counter < len(debt_info):
             if debt_info[counter].get_attribute('class') == 'first':
+                # class first хранит информацию о имени должника.
+                # => Следующие 7 ячеек информация о задолженности.
+                # 1 из них - "сервис", возможность немедленной оплаты, её игнорируем
                 tmp_lst: list = debt_info[counter:counter + 4] + debt_info[counter + 5:counter + 8]
-                debts.append(tuple(map(get_text, tmp_lst)))
+                all_debts.append(tuple(map(get_text, tmp_lst)))
 
                 counter += 7
             counter += 1
 
         return True
 
-    def _solve_captcha(self) -> int:
-        """0 - captcha solved/-1 - restart"""
+    def _solve_captcha(self):
+        """Solves Fssp page captcha using tesseract-ocr"""
+        prev_src: str = ''
         while True:
+            # Проверка наличия капчи на странице.
             try:
                 captcha = self.browser.find_element_by_id('capchaVisual')
                 src = captcha.get_attribute('src')
             except NoSuchElementException:
-                # We've logged in
-                return 0
+                # Капчи нет - мы уже авторизованы.
+                break
 
-            try:
-                captcha_btn = self.wait.until(
-                    EC.element_to_be_clickable((By.CSS_SELECTOR, 'input.input-submit-capcha')))
-            except TimeoutException:
-                return 0
+            if src[-1:-10:-1] == prev_src:
+                # Баг - зациклились
+                return -1
 
-            img = urllib.request.urlretrieve(src)
-            captcha_text = get_text_from_captcha(img[0])
+            img: tuple = urllib.request.urlretrieve(src)  # (tmp_filename, )
+            captcha_text: str = get_text_from_captcha(img[0])
             print(captcha_text)
-            # if captcha_text == '':
-            #     # обновить страницу
-            #     self.browser.refresh()
-            #     return -1
 
             input_form = self.browser.find_element_by_id('captcha-popup-code')
             input_form.send_keys(captcha_text)
 
+            # Так как к концу каждой итерации мы ждём заверщения процессинга нашего ответа на капчу,
+            # а в начале мы проверяем наличие капчи - к данному этапу кнопка должна существовать,
+            # и быть доступна.
+            captcha_btn = self.browser.find_element_by_css_selector('input.input-submit-capcha')
             captcha_btn.click()
+
+            prev_src = src[-1:-10:-1]
+            # Сайт слабенький, поэтому иногда приходится ждать до 8 секунд ответа проверки капчи.
+            # Если наш ответ неверен - появится новое src и новая кнопка.
+            # Иначе - мы получим результат запроса(случай истечения времени ожидания).
+            try:
+                self.wait.until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, 'input.input-submit-capcha')))
+            except TimeoutException:
+                break
+
+        return 0
 
     def __del__(self):
         self.browser.quit()
@@ -111,11 +155,12 @@ if __name__ == '__main__':
     potential_debtors: Deque = read_excel_file()
     session = SessionFssp()
     debts = []
+
     while len(potential_debtors) > 0:
         el = potential_debtors.popleft()
-        debt_found: bool = session.get_debts(args=el, debts=debts)
+
+        debt_found: bool = session.get_debts(args=el, all_debts=debts)
         if not debt_found:
-            print('failed')
             potential_debtors.append(el)
 
     write_excel_file(debts)
